ad59b53e7b4dd64c50acfea63b033c545cc5f0f9
Jordan <jordplamondon@gmail.com>
2017-10-19 14:36:33 -0700

    linked scripts.js and got API working

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
-var hello = console.log('hi');
-
-hello();
+$('#article-selection').on('change', function(){
+  
+    $('#stories').empty();
+  
+    var selectVal = $('#article-selection').val();
+  
+    var url = "https://api.nytimes.com/svc/topstories/v2/" + selectVal + ".json";
+    url += '?' + $.param({
+      'api-key': "80315f98187c49169c887403d2c3f766"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function (data) {
+  
+      console.log(data.results);
+  
+      $.each(data.results, function(index, object){
+        console.log(object.title);
+  
+        $('#stories').append(object.title);
+  
+      });
+  
+    }).fail(function (err) {
+  
+      throw err;
+  
+    });
+  
+    
+  });
