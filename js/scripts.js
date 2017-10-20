bb1420d619fbb925b969309904a1104580ec36aa
Jordan <jordplamondon@gmail.com>
2017-10-20 15:36:04 -0700

    some ajax

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,42 @@ $('#article-selection').on('change', function(){
   
     var selectVal = $('#article-selection').val();
   
-    var url = "https://api.nytimes.com/svc/topstories/v2/" + selectVal + ".json";
+    var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectVal + '.json';
     url += '?' + $.param({
       'api-key': "80315f98187c49169c887403d2c3f766"
     });
-    $.ajax({
-      url: url,
-      method: 'GET',
-    }).done(function (data) {
-  
-      console.log(data.results);
-  
-      $.each(data.results, function(index, object){
-        console.log(object.title);
-  
-        $('#stories').append(object.title);
-  
-      });
-  
+   
+    $.ajax({url: url,method: 'GET', 
+  }).done(function (data) {
+
+    console.log(data);
+
+      var articlesArray = data.results;
+
+      if(articlesArray.lenth !== 0){
+        
+        var title
+
+        $.each(articlesArray, function(index, value){
+
+
+          console.log('stories', index, value.title);
+          var title1 = value.title
+          title += '<h2 class="' + title1 + '">' + title1 + '</h2>'     
+        
+        });
+        $('#stories').append(title);
+
+      } else {
+
+      }
+
+
+  // $('#stories').append('<li>' + data.results[5].title + '</li>');
+
     }).fail(function (err) {
   
       throw err;
   
     });
-  
-    
   });
