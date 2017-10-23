49afa3b8c07b1493281e36ae86b890294e4390cd
Jordan <jordplamondon@gmail.com>
2017-10-23 13:17:55 -0700

    multimedia imgs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $('#article-selection').on('change', function(){
   
     $('#stories').empty();
+    $('.loading').show();
   
     var selectVal = $('#article-selection').val();
   
@@ -16,30 +17,41 @@ $('#article-selection').on('change', function(){
 
       var articlesArray = data.results;
 
-      if(articlesArray.lenth !== 0){
+      if(articlesArray.length !== 0){
         
         var title
 
         $.each(articlesArray, function(index, value){
 
-
           console.log('stories', index, value.title);
           var title1 = value.title
-          title += '<h2 class="' + title1 + '">' + title1 + '</h2>'     
-        
+          title += '<h2 class="' + title1 + '">' + title1 + '</h2>';
+          title += '<img src=' + value.multimedia[2].url + '>'; 
+
+          // var articlesArray = data.results.filter(function(item,index){
+          //   return item.multimedia.length;
+          // }).slice(0, 12).forEach(function (item) {
+          //   $('#stories').append(
+
+          
+          // });
+    
+          
+
+      
         });
         $('#stories').append(title);
 
       } else {
 
+
       }
 
 
-  // $('#stories').append('<li>' + data.results[5].title + '</li>');
-
     }).fail(function (err) {
-  
       throw err;
+    }).always(function () {
+      $('.loading').hide();
   
     });
   });
