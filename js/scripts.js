087065159318faf076517c05f68cca3336db5551
Jordan <jordplamondon@gmail.com>
2017-10-23 23:46:43 -0700

    attempt switchClass

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,20 @@
 $('.select-section').on('change', function(){
 
-  // $('logocontainer').switchClass("head-large", "head-small", 1000, "easeOutQuad");
-  // $('topimage').switchClass("logo-large", "logo-small", 1000, "easeOutQuad");
-  
-  $(function() {
-    $('#articles-selection').selectric();
-  });
-
+  // $(function() {
+  //   $('#article-selection').selectric();
+  // });
+ 
+  // $('large-logo-container').switchClass("large-logo-container", "small-logo-container", 1000, "easeOutQuad");
+  // $('large-top-image').switchClass("large-top-image", "small-top-image", 1000, "easeOutQuad");  
+    
     $('.stories').empty();
     $('.loading').show();
-    $('.loading').hide();
-  
+    
     var selectVal = $('.select-section').val();
-  
+    
     var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectVal + '.json';
     url += '?' + $.param({
       'api-key': "80315f98187c49169c887403d2c3f766"
-    });
-   
-    $(function() {
-      $('select').selectric();
     });
     
     $.ajax({url: url,method: 'GET', 
@@ -34,14 +29,14 @@ $('.select-section').on('change', function(){
           var image = item.multimedia[item.multimedia.length - 1].url;
           var url = item.url;
           $('.stories').append('<a class="images" style="background-image: url(' + image + ')" href="' + url + '"' + '>' + '<div class="select-section">' + title + '</div></a>');
-
+          
         })
-
+        
       }).fail(function (err) {
         throw err;
 
       }).always(function () {
         $('.loading').hide();
-
+    
       });
 });
